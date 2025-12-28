c66a3e477b30523468cde51d714c759791664e08
Durodola Abolaji <bolajidurodola@gmail.com>
2025-12-28 13:46:44 +0100

    feat: setting up

---
+++ src/main/java/com/thedurodola/ekolo/EKoloApplication.java
@@ -0,0 +1,13 @@
+package com.thedurodola.ekolo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EKoloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EKoloApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/thedurodola/ekolo/EKoloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thedurodola.ekolo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EKoloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
